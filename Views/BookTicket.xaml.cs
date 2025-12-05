2d48c7c35a97645a734d99545029a35b280798b1
Nguyễn Hồng Quân <hongquan272005@gmail.com>
2025-12-05 15:23:37 +0700

    Quan

---
+++ Views/BookTicket.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+using Theater_Management_FE.Helpers;
+
+namespace Theater_Management_FE.Views
+{
+    public partial class BookTicket : Window
+    {
+        public BookTicket()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/BookedTicket.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows;
+using Theater_Management_FE.Helpers;
+
+namespace Theater_Management_FE.Views
+{
+    public partial class BookedTicket : Window
+    {
+        public BookedTicket()
+        {
+            InitializeComponent();
+            WindowHelper.InitializeWindow(this);
+        }
+    }
+}
